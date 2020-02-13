013fbabb3a28c5fd660cb17518b7dad53deca4f0
RGBsrs <50082968+RGBsrs@users.noreply.github.com>
2020-02-13 19:38:12 +0200

    Правки вида

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIR = [STATIC_DIR]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
