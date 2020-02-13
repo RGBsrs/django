71f7714c809e572f3c7aaa7da15ce8b016351565
RGBsrs <50082968+RGBsrs@users.noreply.github.com>
2020-02-13 14:39:25 +0200

    начал правки

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'django_movie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -4,12 +4,9 @@ from django.views.generic.base import View
 from .models import Movie
 
 
-
-
-
-
 class MoviesView(View):
     """MoviesView"""
+
     def get(self, request):
         movies = Movie.objects.all()
-        return render(request, "movies/movie_list.html", {"movie_list": movies})
+        return render(request, "movies/movies.html", {"movie_list": movies})
