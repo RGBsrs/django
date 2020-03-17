fd0673e8924e4d7c2a8aa4e057fad1554db8ce10
RGBsrs <50082968+RGBsrs@users.noreply.github.com>
2020-03-17 20:12:41 +0200

    lesson 12 starts

--- django_movie/movies/admin.py
+++ django_movie/movies/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
 
@@ -14,12 +15,22 @@ class ReviewInline(admin.TabularInline):
     readonly_fields = ("name", "email")
 
 
+class MovieShotsInline(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ("get_image", )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60">')
+    get_image.short_desription = "Image"
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ("title", "category", "url", "draft")
     list_filter = ("category", "year")
     search_fields = ("title", "category__name")
-    inlines = [ReviewInline]
+    inlines = [MovieShotsInline, ReviewInline]
     save_on_top = True
     save_as = True
     list_editable = ("draft",)
@@ -31,11 +42,24 @@ class ReviewAdmin(admin.ModelAdmin):
     readonly_fields = ("name", "email")
 
 
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("name", "age", "get_image")
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60">')
+    get_image.short_desription = "Image"
+
+
 #admin.site.register(Category, CategoryAdmin)
 admin.site.register(Genre)
 # admin.site.register(Movie)
 admin.site.register(MovieShots)
-admin.site.register(Actor)
+# admin.site.register(Actor)
 admin.site.register(Rating)
 admin.site.register(RatingStar)
 # admin.site.register(Reviews)
+
+
+admin.site.site_title = "My Movies"
+admin.site.site_header = "My Movies"
