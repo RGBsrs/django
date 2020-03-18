8346894325c7b1634a9f2cbb83a09aecba41509e
RGBsrs <50082968+RGBsrs@users.noreply.github.com>
2020-03-18 17:46:19 +0200

    added ckeditor&youtube

--- django_movie/movies/admin.py
+++ django_movie/movies/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
+from django import forms
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
+
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(
+        label="Описание", widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 
 @admin.register(Category)
@@ -34,6 +47,7 @@ class MovieAdmin(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     list_editable = ("draft",)
+    form = MovieAdminForm
 
 
 @admin.register(Reviews)
