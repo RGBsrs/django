f2b2c0855c1b7ebaf6ba6e929226bf467c52d8f5
RGBsrs <50082968+RGBsrs@users.noreply.github.com>
2020-06-13 15:10:00 +0300

    fiters not work

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -2,18 +2,28 @@ from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from .models import Movie, Category, Actor
+from .models import Movie, Actor, Genre
 from .forms import ReviewForm
 
 
-class MoviesView(ListView):
+class GenreYear:
+    """Years and genres of films"""
+
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MoviesView(GenreYear, ListView):
     """MoviesView"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     template_name = "movies/movies.html"
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     # Detailed view
     model = Movie
     slug_field = 'url'
@@ -35,8 +45,17 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     """ Information about actor """
     model = Actor
     template_name = 'movies/actor.html'
     slug_field = "name"
+
+
+class FilterMoviesView(GenreYear, ListView):
+    """Film filter"""
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            year__in=self.request.GET.getlist("year"), genres__in=self.request.GET.getlist("genres"))
+        return queryset
